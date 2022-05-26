a625133af19bff94fa7862f5226807f21af54463
karlarn <karlarnichols@gmail.com>
2022-05-26 14:58:35 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Plan_Your_Heist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
